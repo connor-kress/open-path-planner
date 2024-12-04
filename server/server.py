10d10aa21b79939e4c7e9d5fd522a976e67e2053
Connor Kress <ckress@ufl.edu>
2024-12-04 14:48:03 -0500

    Accept query parameters for start and finish coords

--- server/server.py
+++ server/server.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.get("/")
+@app.get("/path")
 def test():
-    # returns json
-    return {"members": ["Member1", "Member2", "Member3"]}
+    try:
+        lat1 = float(request.args["lat1"])
+        lon1 = float(request.args["lon1"])
+        lat2 = float(request.args["lat2"])
+        lon2 = float(request.args["lon2"])
+    except (ValueError, TypeError, KeyError):
+        return {"error": "Invalid or missing query parameters"}
+    # start = (lat1, lon1)
+    # finish = (lat2, lon2)
+    return {"start": [lat1, lon1], "finish": [lat2, lon2]}
 
 
 if __name__ == "__main__":
