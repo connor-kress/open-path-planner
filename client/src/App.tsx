0ac0d5729c03032fb172fbdc67a082382915fab9
Connor Kress <ckress@ufl.edu>
2024-12-04 02:30:50 -0500

    Point-based route selection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,70 +1,111 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Icon, LatLngExpression } from 'leaflet';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents
+} from 'react-leaflet'
+
 import 'leaflet/dist/leaflet.css'
+import './App.css'
 
 function App() {
-  const [data, setData] = useState(null);
-  async function fetchData() {
-    console.log("Fetching data");
-    let res = null;
-    try {
-      res = await fetch("http://localhost:5000");
-      if (!res.ok) throw new Error("Response code is not 200");
-    } catch (err) {
-      console.log("Error fetching data");
-      return;
-    }
-    console.log("Done fetching data");
-    let jsonData = null;
-    try {
-      jsonData = await res.json();
-    } catch (err) {
-      const invalidJson = await res.text();
-      console.log(`Invalid json ${invalidJson}`);
-      return;
-    }
-    console.log(jsonData);
-    setData(jsonData);
+  const [isStart, setIsStart] = useState(true);
+
+  function toggleStartSwitch() {
+    if (!isStart) setIsStart(true);
   }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  function toggleEndSwitch() {
+    if (isStart) setIsStart(false);
+  }
+
+  const [start, setStart] = useState<LatLngExpression | null>(null);
+  const [finish, setFinish] = useState<LatLngExpression | null>(null);
+
+  function handleMapClick(e: { latlng: LatLngExpression }) {
+    if (isStart) {
+      setStart(e.latlng);
+    } else {
+      setFinish(e.latlng);
+    }
+  }
 
   return (
     <>
-      <h1>Data:</h1>
-      {data ? (
-        <p>Data loaded</p>
-      ) : (
-        <p>loading...</p>
-      )}
-      <MyMap />
+      <h2>Select Route:</h2>
+      <label>
+        Start
+        <input
+          type="checkbox"
+          checked={isStart}
+          onChange={toggleStartSwitch}
+        />
+      </label>
+      <br/>
+      <label>
+        Finish
+        <input
+          type="checkbox"
+          checked={!isStart}
+          onChange={toggleEndSwitch}
+        />
+      </label>
+      <MyMap
+        start={start}
+        finish={finish}
+        handleMapClick={handleMapClick}
+      />
     </>
   )
 }
 
-const destIcon = new Icon({
-  iconUrl: "/pin.png",
+const redPin = new Icon({
+  iconUrl: "/red-pin.png",
   iconSize: [38, 38], // measured in pixels
   iconAnchor: [38/2, 38], // anchor in the bottom middle
 });
 
-function MyMap() {
-  const position: LatLngExpression = [29.648643, -82.349709];
+const purplePin = new Icon({
+  iconUrl: "/purple-pin.png",
+  iconSize: [38, 38],
+  iconAnchor: [38/2, 38],
+});
+
+function MyMap({ start, finish, handleMapClick }: {
+  start: LatLngExpression | null,
+  finish: LatLngExpression | null,
+  handleMapClick: (e: {latlng: LatLngExpression}) => void,
+}) {
+  const centerPos: LatLngExpression = [29.648643, -82.349709];
+
+  function MapClickHandler() {
+    useMapEvents({
+      click: handleMapClick,
+    });
+    return null;
+  }
+
   return (
-    <MapContainer style={{height: "100vh"}} center={position} zoom={13} scrollWheelZoom={true}>
+    <MapContainer style={{height: "100vh"}} center={centerPos}
+                  zoom={16} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position} icon={destIcon}>
-        <Popup>
-          You can put any <code>html</code> in here <br/>
-          <button>Test Btn</button>
-        </Popup>
-      </Marker>
+      {start != null &&
+        <Marker position={start} icon={redPin} key={0}>
+          <Popup>Start</Popup>
+        </Marker>
+      }
+      {finish != null &&
+        <Marker position={finish} icon={purplePin} key={1}>
+          <Popup>Finish</Popup>
+        </Marker>
+      }
+      <MapClickHandler />
     </MapContainer>
   );
 }
