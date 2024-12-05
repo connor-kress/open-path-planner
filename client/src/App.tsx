3702d799664255a472eee7e0a97e2c7c44c8b944
Connor Kress <ckress@ufl.edu>
2024-12-04 19:10:57 -0500

    Fetch and display path on frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
-import { Icon, LatLngExpression } from 'leaflet';
+import { Icon, LatLng, LatLngExpression } from 'leaflet';
 import {
   MapContainer,
   Marker,
+  Polyline,
   Popup,
   TileLayer,
   useMapEvents
@@ -10,6 +11,14 @@ import {
 
 import 'leaflet/dist/leaflet.css'
 import './App.css'
+
+async function fetchPathData(start: LatLng, finish: LatLng) {
+  console.log(start);
+  const url = `http://localhost:5000/api/path?lat1=${start.lat}&lon1=${start.lng}&lat2=${finish.lat}&lon2=${finish.lng}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+}
 
 function App() {
   const [isStart, setIsStart] = useState(true);
@@ -33,6 +42,21 @@ function App() {
     }
   }
 
+  const [pathGeoData, setPathGeoData] = useState<[number, number][]| null>(null);
+  const [pathDataLoading, setPathDataLoading] = useState(false);
+
+  async function handleFindPath() {
+    if (start === null || finish === null) {
+      alert("Please select both endpoints by selecting the start and then finish checkboxes!");
+      return;
+    }
+    setPathDataLoading(true);
+    const data = await fetchPathData(start as LatLng, finish as LatLng);
+    setPathGeoData(data.geoPath);
+    setPathDataLoading(false);
+    console.log(data);
+  }
+
   return (
     <>
       <h2>Select Route:</h2>
@@ -53,10 +77,16 @@ function App() {
           onChange={toggleEndSwitch}
         />
       </label>
+      <br/>
+      <button onClick={handleFindPath}>
+        Find path
+      </button>
+      {pathDataLoading && <span> Loading...</span>}
       <MyMap
         start={start}
         finish={finish}
         handleMapClick={handleMapClick}
+        pathGeoData={pathGeoData}
       />
     </>
   )
@@ -74,10 +104,11 @@ const purplePin = new Icon({
   iconAnchor: [38/2, 38],
 });
 
-function MyMap({ start, finish, handleMapClick }: {
+function MyMap({ start, finish, handleMapClick, pathGeoData }: {
   start: LatLngExpression | null,
   finish: LatLngExpression | null,
   handleMapClick: (e: {latlng: LatLngExpression}) => void,
+  pathGeoData: [number, number][] | null,
 }) {
   const centerPos: LatLngExpression = [29.648643, -82.349709];
 
@@ -95,15 +126,19 @@ function MyMap({ start, finish, handleMapClick }: {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {start != null &&
+      {start !== null &&
         <Marker position={start} icon={redPin} key={0}>
           <Popup>Start</Popup>
         </Marker>
       }
-      {finish != null &&
+      {finish !== null &&
         <Marker position={finish} icon={purplePin} key={1}>
           <Popup>Finish</Popup>
         </Marker>
+      }
+      {pathGeoData !== null &&
+        <Polyline positions={pathGeoData} color="blue"
+                  weight={4} opacity={0.7} />
       }
       <MapClickHandler />
     </MapContainer>
